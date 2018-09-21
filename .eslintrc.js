459b25a53bcad77daefc5b6b7dd05225bc189d10
FedeG <federicogonzalez761@gmail.com>
2018-09-21 20:13:06 -0300

    Add eslint configuration for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
-    extends: "airbnb",
-    globals: {
-      document: true
+  extends: "airbnb",
+  globals: {
+    document: true
+  },
+  overrides: [{
+    files: [
+      "**/*.test.js",
+      "**/*.test.jsx"
+    ],
+    env: {
+      jest: true
+    },
+    plugins: ["jest"],
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error"
     }
-  };
+  }]
+}
